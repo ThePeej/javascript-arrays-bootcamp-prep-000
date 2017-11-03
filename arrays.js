07649ca1576c38ff8acaef51509d8a856b5e704a
PJ Frias <peejfrias@gmail.com>
2017-11-03 02:31:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = ["snickers" , "hundred grand" , "kitkat" , "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningofArray(array, element) {
+  array.push(element)
+  return array
+}
