fb33feebd2601f62d0bdc00a0408e2bcf6206253
PJ Frias <peejfrias@gmail.com>
2017-11-03 02:33:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,16 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningofArray(array, element) {
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
 }
